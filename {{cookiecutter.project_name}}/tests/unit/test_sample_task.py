8ee0e4c75d9ca72daecadece65ebcad7d2780e9f
Santiago Armstrong <santiago.armstrong@cencosud.cl>
2023-10-03 11:54:07 -0300

    UPDATE: docstring in sample test

--- {{cookiecutter.project_name}}/tests/unit/test_sample_task.py
+++ {{cookiecutter.project_name}}/tests/unit/test_sample_task.py
@@ -30,6 +30,7 @@ from tests.unit.utils import (
 class TestSampleTask:
     @pytest.fixture
     def setUp(self, spark: SparkSession):
+        # This is an example of how to set up a configuration file to test a task
         self.config = {
             "experiment": "/Shared/dbx/{{cookiecutter.package_name}}/dev_{{cookiecutter.package_name}}",
             "model_kwargs": {
@@ -38,20 +39,24 @@ class TestSampleTask:
                 "tol": 0.0001,
             },
             "database": "dev",
-            "table": "sample_data",
-            "path": "",
+            "input": {"table": "sample_transactions","path": ""},
+            "output": {"table": "sample_data","path": ""},
             "execution_date": "2023-09-01",
         }
 
         # update conf file
         warehouse_dir = spark.conf.get("spark.hive.metastore.warehouse.dir")
 
-        self.config["path"] = os.path.join(
-            warehouse_dir, "sample_data"
+        self.config["output"]["path"] = os.path.join(
+            warehouse_dir, self.config["output"]["table"]
+        )
+        self.config["input"]["path"] = os.path.join(
+            warehouse_dir, self.config["input"]["table"]
         )
         # create database
         create_database(spark, self.config)
 
+        # This is an example of how to set up a SQL table with data to test a task
         # Define the schema for the DataFrame
         self.transactions_schema = StructType(
             [
@@ -62,37 +67,27 @@ class TestSampleTask:
                 StructField("date", StringType(), True),
             ]
         )
-
-        # TRANSACTION DATA
-        # 5 records (dates) for each sk_material
         df_transactions = pd.DataFrame(
             data={
-                "sku": ["15010201"] * 5
-                + ["15010202"] * 5
-                + ["15010203"] * 5,
-                "store": "158",
+                "sku": "314",
+                "store": "S101",
                 "date": [
                     "20210101",
                     "20210102",
                     "20210103",
                     "20210104",
                     "20210105",
-                ]
-                * 3,
-                "sales": [10 * 10, 9 * 11, 8 * 12, 7 * 13, 6 * 14]
-                * 3,  # PxQ
-                "units": [10, 11, 12, 13, 14] * 3,
+                ],
+                "sales": [10 * 10, 9 * 11, 8 * 12, 7 * 13, 6 * 14],
+                "units": [10, 11, 12, 13, 14],
             }
         )
-
         df_transactions = df_transactions[
             self.transactions_schema.fieldNames()
         ]
-
         self.df_transactions = spark.createDataFrame(
             df_transactions, schema=self.transactions_schema
         )
-
         # write dataframe to delta table
         write_external_delta_table(
             spark,
@@ -100,28 +95,28 @@ class TestSampleTask:
             self.transactions_schema,
             os.path.join(
                 warehouse_dir,
-                self.config["input"]["transactions_data"]["table"],
+                self.config["input"]["table"],
             ),
             self.config["database"],
-            self.config["input"]["transactions_data"]["table"],
+            self.config["input"]["table"],
             partition_cols=["date"],
         )
 
-        # Excepted output
+        # This is the excepted output of the task
         data = [
             (
-                12.0,
-                "101",
+                1.0,
+                "foo",
                 date(2023, 9, 1),
             ),
             (
-                12.0,
-                "101",
+                2.0,
+                "bar",
                 date(2023, 9, 1),
             ),
             (
-                12.0,
-                "101",
+                3.0,
+                "baz",
                 date(2023, 9, 1),
             ),
         ]
@@ -137,19 +132,19 @@ class TestSampleTask:
 
 
     @pytest.mark.spark
-    def test_output_ownelasticity(
+    def test_task_output(
         self, setUp, spark: SparkSession
     ):
         # Check if output tables exist
         df = spark.read.format("delta").load(
-            self.config["path"]
+            self.config["output"]["path"]
         )
         assert df.count() > 0
 
         # Check if output table is as expected
         assert_pyspark_df_equal(
-            df, self.expected_df, atol=0.1
-        )  # since there is randomness in the algorithm, we use a tolerance of 0.1
+            df, self.expected_df, atol=0.0
+        )
 
     @pytest.mark.spark
     @pytest.mark.mlflow
