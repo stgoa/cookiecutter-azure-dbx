fa9c0b90c75c8a786953a6535767423a4d9338c9
Santiago Armstrong <santiago.armstrong@cencosud.cl>
2023-12-26 16:36:04 -0300

    REMOVE: init scrip logic is no longer necessary

--- {{cookiecutter.project_name}}/resources/init_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -n "$EXTRA_INDEX_URL" ]; then
-  PIP=$(which pip)
-  for varname in ${!PRIVATE_PACKAGE_*}
-    do
-        echo "installing ${!varname}"
-        $PIP install --extra-index-url ${EXTRA_INDEX_URL} ${!varname}
-  done
-fi
